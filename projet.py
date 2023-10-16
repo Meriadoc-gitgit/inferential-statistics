5b21afea27cd0b045b6633eb43d7b5fefa859544
Thuy Duong <vuhoang.thduong@gmail.com>
2023-10-16 22:23:45 +0200

    update project

--- projet.py
+++ projet.py
@@ -4,11 +4,18 @@
 # Hoang Thuy Duong VU
 # HalimatoudDIALLO
 ####
+
+# Import necessary libraries
 import numpy as np
+
+# Import utils
+from utils import *
 
 
 #####
-# QUESTION 1.1
+# CLASSIFICATION A PRIORI
+#####
+# QUESTION 1.1 - CALCUL DE PROBA A PRIORI
 #####
 
 def getPrior(df) :
@@ -20,3 +27,38 @@ def getPrior(df) :
     "min5pourcent" :  mu-1.96*std,
     "max5pourcent" : mu+1.96*std
   }
+
+
+
+#####
+# QUESTION 1.2 - POO DANS LA HIERACHIE DES `CLASSIFIER`
+#####
+class AprioriClassifier(AbstractClassifier) : 
+  pass
+
+
+
+
+class ML2DClassifier(AprioriClassifier) : 
+  pass
+
+class MAP2DClassifier(AprioriClassifier) : 
+  pass
+
+class MLNaiveBayesClassifier(AprioriClassifier) : 
+  pass
+
+class MAPNaiveBayesClassifier(AprioriClassifier) : 
+  pass
+
+class MAPTANlassifier(AprioriClassifier) : 
+  pass
+
+
+
+
+class ReducedMLNaiveBayesClassifier(MLNaiveBayesClassifier) : 
+  pass
+
+class ReducedMAPNaiveBayesClassifier(MLNaiveBayesClassifier) : 
+  pass
